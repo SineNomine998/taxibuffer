bde9c9c0f864c18ef531bada5a9102a50aa876e2
Enes Yavuz Kozan <eykozan2004@gmail.com>
2025-09-24 10:07:39 +0200

    Name fixed + Control Dashboard logo fixed

--- taxibuffer/asgi.py
+++ taxibuffer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for taxibuffers project.
+ASGI config for taxibuffer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxibuffers.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxibuffer.settings")
 
 application = get_asgi_application()
