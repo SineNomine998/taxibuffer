60f21ef21e29a42e5efbdaa5839f207d121716fc
Enes Yavuz Kozan <eykozan2004@gmail.com>
2026-07-22 15:01:13 +0200

    Fix Android related issues/warnings + make it more production-ready

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.mobile_app"
+    namespace = "com.codingthecurbs.taxibuffer"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +16,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.codingthecurbs.taxibuffer"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
--- mobile_app/android/app/src/main/kotlin/com/codingthecurbs/taxibuffer/MainActivity.kt
+++ mobile_app/android/app/src/main/kotlin/com/codingthecurbs/taxibuffer/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mobile_app
+package com.codingthecurbs.taxibuffer
 
 import io.flutter.embedding.android.FlutterActivity
 
